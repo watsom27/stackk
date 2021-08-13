d93a9c73b84346a96ce71ae4aa7b1bbf59133d77
Michael Watson <michael.watson@wrenkitchens.com>
2021-08-13 20:03:57 +0100

    Starting thing

--- src/components/ItemComponent.tsx
+++ src/components/ItemComponent.tsx
@@ -1,4 +1,5 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import styled from 'styled-components';
+import React, { Dispatch, SetStateAction, useState, useEffect, useRef } from 'react';
 import { db } from '~data/Db';
 import { Item } from '~data/Item';
 
@@ -7,6 +8,32 @@ interface ItemComponentProps {
     isFirst?: boolean;
     setItems: Dispatch<SetStateAction<Item[]>>;
 }
+
+interface Position {
+    x: number;
+    y: number;
+}
+
+interface PositionedThingProps extends Position {
+    isMouseDown: boolean;
+    width: number;
+}
+
+const PositionedThing = styled.div<PositionedThingProps>`
+    ${(props) => {
+        if (props.isMouseDown) {
+            return `
+                position: fixed;
+                z-index: 100;
+                left: ${props.x}px;
+                top: ${props.y}px;
+                width: ${props.width}px;
+            `;
+        }
+
+        return '';
+    }}
+`;
 
 function Controls({ item, isFirst, setItems }: ItemComponentProps): JSX.Element {
     const btnDoneOnClick = async () => {
@@ -51,14 +78,47 @@ function Controls({ item, isFirst, setItems }: ItemComponentProps): JSX.Element 
 }
 
 export function ItemComponent({ item, isFirst, setItems }: ItemComponentProps): JSX.Element {
-    const className = `item ${isFirst ? 'first' : ''}`;
+    const [mouseDown, setMouseDown] = useState<boolean>(false);
+    const [mousePosition, setMousePosition] = useState<Position>({ x: 0, y: 0 });
+    const className = `item ${mouseDown ? 'down' : ''} ${isFirst ? 'first' : ''} `;
+    const [width, setWidth] = useState<number>();
+    const ref = useRef();
+
+    useEffect(() => {
+        window.addEventListener('mousemove', (event) => {
+            setMousePosition({
+                x: event.clientX,
+                y: event.clientY,
+            });
+        });
+
+        if (ref.current) {
+            setWidth(ref.current.offsetWidth);
+        }
+
+        window.addEventListener('resize', () => {
+            if (ref.current) {
+                setWidth(ref.current.offsetWidth);
+            }
+        });
+    }, []);
 
     return (
-        <div className={className}>
-            <p className='value'>
-                {item.value}
-            </p>
-            <Controls item={item} isFirst={isFirst} setItems={setItems} />
+        <div className={`mouse-down-background ${isFirst ? 'first' : ''} ${mouseDown ? 'down' : ''}`}>
+            <PositionedThing
+                className={className}
+                onMouseDown={() => setMouseDown(true)}
+                onMouseUp={() => setMouseDown(false)}
+                x={mousePosition.x}
+                y={mousePosition.y}
+                isMouseDown={mouseDown}
+                width={width}
+            >
+                <p className='value'>
+                    {item.value}
+                </p>
+                <Controls item={item} isFirst={isFirst} setItems={setItems} />
+            </PositionedThing>
         </div>
     );
 }
