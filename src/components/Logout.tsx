75606b17a630f510028ba9fde3810eca30c9deef
Michael Watson <68954833+watsom27@users.noreply.github.com>
2021-11-21 18:32:17 +0000

    Remove login restriction on /release. Also updated logout button to show login if applicable (#36)

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { URLs } from '~config/URLs';
 import { LoginService } from '~service/loginService';
 
 export function Logout(): JSX.Element {
-    const onClick = () => LoginService.logout();
+    const history = useHistory();
 
-    return <div className='logout logout-pad' onClick={onClick} />;
+    const isLoggedIn = LoginService.isLoggedIn();
+    const action = isLoggedIn ? 'out' : 'in';
+    const onClick = isLoggedIn
+        ? () => LoginService.logout()
+        : () => history.push(`${URLs.login}?returnAddr=${URLs.release}`);
+
+    return (
+        <div
+            className={`logout-button logout-pad ${action}`}
+            onClick={onClick}
+        />
+    );
 }
