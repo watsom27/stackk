873aedef6cb34dce7b862f79fa0b91e4952041f9
michael <watty.wat@me.com>
2022-01-10 23:34:02 +0000

    Enable code splitting on pages

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,14 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, lazy, Suspense } from 'react';
 import { BrowserRouter, Redirect, Route, Switch, useLocation } from 'react-router-dom';
 import { URLs } from '~config/URLs';
-import { Account } from '~pages/Account';
-import { Login } from '~pages/Login';
-import { NotFound } from '~pages/NotFound';
 import { ReleaseNotes } from '~pages/ReleaseNotes';
-import { Reset } from '~pages/Reset';
-import { View } from '~pages/View';
 import { LoginService } from '~service/loginService';
 import { KeyboardShortcuts } from '~pages/KeyboardShortcuts';
+import { Wrapper } from './Wrapper';
+
+// React lazy to enable code splitting on pages
+const Account = lazy(() => import('~pages/Account'));
+const Login = lazy(() => import('~pages/Login'));
+const NotFound = lazy(() => import('~pages/NotFound'));
+const Reset = lazy(() => import('~pages/Reset'));
+const View = lazy(() => import('~pages/View'));
 
 const OPEN_URLS: string[] = [URLs.login, URLs.reset, URLs.release];
 
@@ -50,38 +53,40 @@ export function Router(): JSX.Element {
     return loginInitialised
         ? (
             <BrowserRouter>
-                <LoginRedirect />
-                <Switch>
-                    <Route path={URLs.view}>
-                        <View />
-                    </Route>
+                <Suspense fallback={<Wrapper />}>
+                    <LoginRedirect />
+                    <Switch>
+                        <Route path={URLs.view}>
+                            <View />
+                        </Route>
 
-                    <Route path={URLs.login}>
-                        <Login />
-                    </Route>
+                        <Route path={URLs.login}>
+                            <Login />
+                        </Route>
 
-                    <Route path={URLs.account}>
-                        <Account />
-                    </Route>
+                        <Route path={URLs.account}>
+                            <Account />
+                        </Route>
 
-                    <Route path={URLs.reset}>
-                        <Reset />
-                    </Route>
+                        <Route path={URLs.reset}>
+                            <Reset />
+                        </Route>
 
-                    <Route path={URLs.release}>
-                        <ReleaseNotes />
-                    </Route>
+                        <Route path={URLs.release}>
+                            <ReleaseNotes />
+                        </Route>
 
-                    <Route path={URLs.keyboardShortcuts}>
-                        <KeyboardShortcuts />
-                    </Route>
+                        <Route path={URLs.keyboardShortcuts}>
+                            <KeyboardShortcuts />
+                        </Route>
 
-                    <Route exact path='/'>
-                        <Redirect to={URLs.view} />
-                    </Route>
+                        <Route exact path='/'>
+                            <Redirect to={URLs.view} />
+                        </Route>
 
-                    <NotFound />
-                </Switch>
+                        <NotFound />
+                    </Switch>
+                </Suspense>
             </BrowserRouter>
         )
         : <></>;
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export function NotFound(): JSX.Element {
+export default function NotFound(): JSX.Element {
     return (
         <>
             <h1>Page Not Found</h1>
