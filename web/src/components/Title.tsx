80312c6c8545d4519343f73ab60b1bbc1218290b
Michael Watson <watty.wat@me.com>
2023-04-13 10:21:59 +0100

    Fix lint

--- web/src/components/Title.tsx
+++ web/src/components/Title.tsx
@@ -14,7 +14,12 @@ export function Title({ title, showReturn }: TitleProps): JSX.Element {
             {showReturn ? <Return /> : <SettingsIcon />}
             <div>
                 <h3>{title}</h3>
-                <h5 className="error">IMPORTANT: Stackk will go offline on April 16th 2023, please move all your todos to <a href="https://planner.backup-team.co.uk">Life Planner</a> to avoid loss</h5>
+                <h5 className='error'>
+                    IMPORTANT: Stackk will go offline on April 16th 2023, please move all your todos to
+                    <a href='https://planner.backup-team.co.uk'>Life Planner</a>
+                    {' '}
+                    to avoid loss
+                </h5>
             </div>
             <Logout />
         </div>
