078af42963af3c0fdb6f1f966ec9d9ffe98a8580
Michael Watson <68954833+watsom27@users.noreply.github.com>
2023-04-13 10:23:31 +0100

    Add depreciation warning (#50)
    * Add depreciation warning
    
    * Fix lint

--- web/src/components/Title.tsx
+++ web/src/components/Title.tsx
@@ -12,7 +12,15 @@ export function Title({ title, showReturn }: TitleProps): JSX.Element {
     return (
         <div className='title'>
             {showReturn ? <Return /> : <SettingsIcon />}
-            <h3>{title}</h3>
+            <div>
+                <h3>{title}</h3>
+                <h5 className='error'>
+                    IMPORTANT: Stackk will go offline on April 16th 2023, please move all your todos to
+                    <a href='https://planner.backup-team.co.uk'>Life Planner</a>
+                    {' '}
+                    to avoid loss
+                </h5>
+            </div>
             <Logout />
         </div>
     );
